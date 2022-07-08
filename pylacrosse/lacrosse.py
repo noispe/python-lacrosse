4433bdc9b0df7239c7ba7cc83ffb6e0bc3fc44e9
Bernhard Spindler <bernhard.spindler@mailbox.org>
2022-07-08 15:42:02 +0200

    Comple rework of LaCrosseSensor class.

--- pylacrosse/lacrosse.py
+++ pylacrosse/lacrosse.py
@@ -20,6 +20,7 @@ import logging
 import re
 import threading
 
+
 _LOGGER = logging.getLogger(__name__)
 
 """
@@ -42,7 +43,6 @@ _LOGGER = logging.getLogger(__name__)
 
 class LaCrosse(object):
 
-    sensors = {}
     _registry = {}
     _callback = None
     _serial = None
@@ -51,6 +51,7 @@ class LaCrosse(object):
 
     def __init__(self, port, baud, timeout=2):
         """Initialize the Lacrosse device."""
+        self.sensors = dict()
         self._port = port
         self._baud = baud
         self._timeout = timeout
@@ -193,10 +194,14 @@ class LaCrosse(object):
                 line = line.encode().decode('utf-8')
             except AttributeError:
                 line = line.decode('utf-8')
-
-            if LaCrosseSensor.re_reading.match(line):
+            if LaCrosseSensor.can_parse(line):
+                
                 sensor = LaCrosseSensor(line)
-                self.sensors[sensor.sensorid] = sensor
+                if sensor.sensorid in self.sensors:
+                    self.sensors[sensor.sensorid].update_from(sensor)
+                    sensor = self.sensors[sensor.sensorid]
+                else:
+                    self.sensors[sensor.sensorid] = sensor
 
                 if self._callback:
                     self._callback(sensor, self._callback_data)
@@ -218,29 +223,100 @@ class LaCrosse(object):
 
 
 class LaCrosseSensor(object):
-    """The LaCrosse Sensor class."""
-    # OK 9 248 1 4 150 106
-    re_reading = re.compile(r'OK (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)')
+    """The LaCrosse Sensor Base Class"""
+    re_tempsensor = re.compile('OK 9( \d+)*')
+    re_weathersensor = re.compile('OK WS( \d+)*')
 
-    def __init__(self, line=None):
+    def __init__(self, line=None) -> None:
+        self.measurements = dict()
         if line:
-            self._parse(line)
+            self.parse(line)
 
-    def _parse(self, line):
-        match = self.re_reading.match(line)
+    @staticmethod
+    def can_parse(line:str) -> bool:
+        return LaCrosseSensor.re_tempsensor.match(line) or LaCrosseSensor.re_weathersensor.match(line)
+
+    def parse(self, line:str):
+        self.line = line #TODO: remove (was added just for debugging)
+        #try temperature / humidity sensor
+        match = self.re_tempsensor.match(line)
         if match:
-            data = [int(c) for c in match.group().split()[1:]]
-            self.sensorid = data[1]
-            self.sensortype = data[2] & 0x7f
-            self.new_battery = True if data[2] & 0x80 else False
-            self.temperature = float(data[3] * 256 + data[4] - 1000) / 10
-            self.humidity = data[5] & 0x7f
-            self.low_battery = True if data[5] & 0x80 else False
+            #line matches temperature sensor
+            msg = [int(c) for c in match.group().split()[2:]]
+            self.sensorid = msg[0]
+            channel = '' if msg[1] & 0x7F == 1 else str(msg[1] & 0x7F)
+            self.sensor_type = msg[1] & 0x7F
+            self.new_battery = True if msg[1] & 0x80 else False
+            self.low_battery = True if msg[4] & 0x80 else False
+            self.update_value('temperature' + channel, float(msg[2] * 256 + msg[3] - 1000) / 10, '°C')
+            if msg[4] <= 100:
+                self.update_value('humidity' + channel, msg[4] & 0x7F, '%')
+        match = self.re_weathersensor.match(line)
+        if match:
+            #line matches weather station
+            msg = [int(c) for c in match.group().split()[2:]]
+            msglen = len(msg)
+            self.sensorid = msg[0]
+            self.sensor_type = msg[1] #1=TX22, 2=NodeSensor, 3=WS1080, 4=LaCrosseGateway, 5=Universal Sensor, 6=FineOffset(WH24, WH65B, HP1000)
+            if not (msg[2] == 0xFF and msg[3] == 0xFF):
+                self.update_value('temperature',float(msg[2] * 256 + msg[3] - 1000) / 10,'°C')
+            if msg[4] != 0xFF:
+                self.update_value('humidity', msg[4], '%')
+            if not (msg[5] == 0xFF and msg[6] == 0xFF):
+                rain = msg[5] * 256 + msg[6]
+                if self.sensor_type == 6: rain /= 10
+                self.update_value('rain', rain , 'mm')
+            if not (msg[7] == 0xFF and msg[8] == 0xFF):
+                self.update_value('winddirection', float(msg[7] * 256 + msg[8]) / 10, '°')
+            if not (msg[9] == 0xFF and msg[10] == 0xFF):
+                self.update_value('windspeed', float(msg[9] * 256 + msg[10]) / 10, 'm/s')
+            if not (msg[11] == 0xFF and msg[12] == 0xFF):
+                self.update_value('windgust', float(msg[11] * 256 + msg[12]) / 10, 'm/s')
+            if self.sensor_type != 6: 
+                if msglen > 15 and not (msg[14] == 0xFF and msg[15] == 0xFF):
+                    pressure = msg[14] * 256 + msg[15]
+                    if pressure > 5000: pressure /= 10
+                    self.update_value('pressure', pressure, 'hPa')
+            else: #sensor_type = 6
+                if msg[14] != 0xFF:
+                    uv = msg[14]
+                    uv_upper = [432, 851, 1210, 1570, 2017, 2450, 2761, 3100, 3512, 3918, 4277, 4650, 5029]
+                    uv_index = 0
+                    while uv_index < 13 and uv > uv_upper[uv_index]:
+                        uv_index += 1
+                    self.update_value('uv_index', uv_index)
+                if msg[15] != 0xFF:
+                    self.update_value('lux', (msg[15] * 65536 + msg[16] * 256 + msg[17])/10, 'lux')
+            if msglen > 18 and not(msg[16] == 0xFF and msg[17] == 0xFF and msg[18]==0xFF):
+                self.update_value('gas1', msg[16] * 65536 + msg[17] * 256 +  msg[18]) 
+            if msglen > 21 and not(msg[19] == 0xFF and msg[20] == 0xFF and msg[21]==0xFF):
+                self.update_value('gas2', msg[19] * 65536 + msg[20] * 256 +  msg[21]) 
+            if msglen > 24 and not(msg[22] == 0xFF and msg[23] == 0xFF and msg[24]==0xFF):
+                self.update_value('lux', msg[22] * 65536 + msg[23] * 256 +  msg[24], 'lux')
+            if self.sensor_type == 5:
+                if msglen > 25 and msg[25] != 0xFF:
+                    self.update_value('version', msg[25] / 10)
+                if msglen > 26 and msg[26] != 0xFF:
+                    self.update_value('voltage', msg[26] / 10)
+                if msglen > 29 and msg[27] != 0xFF:
+                    self.update_value('debug', msg[27]*65536 + msg[28] * 256 + msg[29])
+            
+            self.low_battery = True if msg[13] & 0x04 else False
+            self.new_battery = True if msg[13] & 0x01 else False
+    
+    def update_value(self, key:str, value, unit:str = '')->None:
+        if key in self.measurements:
+            self.measurements[key]['value']=value
+        else:
+            self.measurements.update({key: {'value': value, 'unit': unit}})
 
-    def __repr__(self):
-        return "id=%d t=%f h=%d nbat=%d" % \
-            (self.sensorid, self.temperature, self.humidity, self.new_battery)
+    def update_from(self, sensor) -> None:
+            self.low_battery = sensor.low_battery
+            self.new_battery = sensor.new_battery
+            self.measurements.update(sensor.measurements)
 
+    def __repr__(self) -> str:
+        return '{}-->id={} type={} new_batt={} low_batt={}: {}'.format(self.line, self.sensorid, self.sensor_type, self.new_battery, self.low_battery, self.measurements)
 
 class SerialPortFactory(object):
     def create_serial_port(self, port):
@@ -250,3 +326,46 @@ class SerialPortFactory(object):
         else:
             from serial import Serial
             return Serial()
+
+#  Temperature Sensor Format
+#  
+#   OK 9 56 1   4   156 37     ID = 56  T: 18.0  H: 37  no NewBatt
+#   OK 9 49 1   4   182 54     ID = 49  T: 20.6  H: 54  no NewBatt
+#   OK 9 55 129 4 192 56       ID = 55  T: 21.6  H: 56  WITH NewBatt 
+#   OK 9 ID XXX XXX XXX XXX
+#   |  | |  |   |   |   |
+#   |  | |  |   |   |   --- Humidity incl. WeakBatteryFlag
+#   |  | |  |   |   |------ Temp * 10 + 1000 LSB
+#   |  | |  |   |---------- Temp * 10 + 1000 MSB
+#   |  | |  |-------------- Sensor type (1 or 2) +128 if NewBatteryFlag
+#   |  | |----------------- Sensor ID
+#   |  |------------------- fix "9"
+#   |---------------------- fix "OK"
+
+# Weather Sensor format
+#   OK WS 60  1   4   193 52    2 88  4   101 15  20   ID=60  21.7°C  52%rH  600mm  Dir.: 112.5°  Wind:15m/s  Gust:20m/s
+#   OK WS ID  XXX TTT TTT HHH RRR RRR DDD DDD SSS SSS GGG GGG FFF PPP PPP
+#   |  |  |   |   |   |   |   |   |   |   |   |   |   |   |   |-- Flags *
+#   |  |  |   |   |   |   |   |   |   |   |   |   |   |   |------ WindGust * 10 LSB (0.0 ... 50.0 m/s)           FF/FF = none
+#   |  |  |   |   |   |   |   |   |   |   |   |   |   |---------- WindGust * 10 MSB
+#   |  |  |   |   |   |   |   |   |   |   |   |   |-------------- WindSpeed  * 10 LSB(0.0 ... 50.0 m/s)          FF/FF = none
+#   |  |  |   |   |   |   |   |   |   |   |   |------------------ WindSpeed  * 10 MSB
+#   |  |  |   |   |   |   |   |   |   |   |---------------------- WindDirection * 10 LSB (0.0 ... 365.0 Degrees) FF/FF = none
+#   |  |  |   |   |   |   |   |   |   |-------------------------- WindDirection * 10 MSB
+#   |  |  |   |   |   |   |   |   |------------------------------ Rain LSB (0 ... 9999 mm)                       FF/FF = none
+#   |  |  |   |   |   |   |   |---------------------------------- Rain MSB
+#   |  |  |   |   |   |   |-------------------------------------- Humidity (1 ... 99 %rH)                        FF = none
+#   |  |  |   |   |   |------------------------------------------ Temp * 10 + 1000 LSB (-40 ... +60 °C)          FF/FF = none
+#   |  |  |   |   |---------------------------------------------- Temp * 10 + 1000 MSB
+#   |  |  |   |-------------------------------------------------- Sensor type (1=TX22IT, 2=NodeSensor, 3=WS1080)
+#   |  |  |------------------------------------------------------ Sensor ID (1 ... 63)
+#   |  |--------------------------------------------------------- fix "WS"
+#   |------------------------------------------------------------ fix "OK"
+
+#   * Flags: 128  64  32  16  8   4   2   1
+#   |   |   |
+#   |   |   |-- New battery
+#   |   |------ ERROR
+#   |---------- Low battery
+#   */
+
